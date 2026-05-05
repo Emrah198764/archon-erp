edc61aad5cfa994d4114549aef7b33ec0e4dc772
Emrah Turk <eturk@ext.teachx.ai>
2026-05-05 12:14:15 +0300

    feat: add CRM email sender preference doctype

---
+++ update_crm/update_crm/doctype/crm_email_sender_preference/test_crm_email_sender_preference.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, digisfer and Contributors
+# See license.txt
+
+import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCRMEmailSenderPreference(FrappeTestCase):
+	pass
